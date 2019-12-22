8e05938f8f48ad4b8eec63f3684ea0fe14af22d9
Michael Klement <mklement0@gmail.com>
2019-12-22 13:45:29 -0500

    Added Homebrew formula.

---
+++ ttab.rb
@@ -0,0 +1,16 @@
+class Ttab < Formula
+  desc "CLI for programmatically opening tabs/windows in Terminal or iTerm"
+  homepage "https://github.com/mklement0/ttab"
+  url "https://github.com/mklement0/ttab/archive/v0.6.1.tar.gz"
+  sha256 "5ccfb01c8798bcc78de09ffaeb586eddd5b93166ddf6101167ac9b3202c6e5e1"
+
+  def install
+    bin.install "bin/ttab"
+    man1.install "man/ttab.1"
+  end
+
+  test do
+    stdout = shell_output "#{bin}/ttab --version"
+    assert_match /ttab v#{version}/, stdout
+  end
+end
